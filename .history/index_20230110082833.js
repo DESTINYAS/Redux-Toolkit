94318720a6bfadd3c8b89b9b2ead376d49560907
DESTINYAS <destiny.ochonogor@gmail.com>
2023-01-10 08:34:24 +0300

    store action and reducer

---
+++ .history/index_20230110082833.js
@@ -0,0 +1,61 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+}
+
+function cakeRestock(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+  iceCream: 100,
+  milk: 50,
+};
+
+// (previousState,action)=>newState
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+        iceCream: state.iceCream - 5,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(cakeRestock(3));
+
+unsubscribe();
