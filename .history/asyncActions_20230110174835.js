322caf732f2bbe30897c14bd18075146c736ffa6
DESTINYAS <destiny.ochonogor@gmail.com>
2023-01-10 17:59:18 +0300

    Async Actions

---
+++ .history/asyncActions_20230110174835.js
@@ -0,0 +1,29 @@
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
+const FETCH_USERS_SUCCEDED = "FETCH_USERS_SUCCEDED";
+const FETCH_USERS_FAILED = "FETCH_USERS_FAILED";
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUESTED,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCEDED,
+    payload: users,
+  };
+};
+
+const fetchUsersFailed = (error) => {
+  return {
+    type: FETCH_USERS_FAILED,
+    payload: error,
+  };
+};
